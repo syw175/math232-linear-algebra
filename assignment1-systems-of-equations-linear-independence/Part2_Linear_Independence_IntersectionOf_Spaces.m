e74c40616887702e289fd8f7eaba699254dd8411
Steven Wong <syw14@sfu.ca>
2022-10-10 21:49:11 -0700

    work to be done on q2

---
+++ assignment1-systems-of-equations-linear-independence/Part2_Linear_Independence_IntersectionOf_Spaces.m
@@ -0,0 +1,25 @@
+% Consider the set of vectors in R5: B1 = {w1, w2, w3, w4, w5, w6} where
+w1 = [-1; 1; 2; 4; 1];
+w2 = [-1; 1; 2; 1; 1];
+w3 = [3; 1; -1; 2; 0];
+w4 = [2; 1; 0; 3; -1];
+w5 = [5; 4; 1; 11; 3];
+w6 = [1; 0; -1; 2; 1];
+
+% Show B1 is a linearly dependent set. Then, demonstrate the conclusion of Theorem
+% 1.2.2: Find a maximal linearly independent set B′1
+% of vectors from B1, and show that
+% the vectors from B1 that are NOT in B′1
+% set are contained in the span of B′1
+% (and hence, that span B1 = span B′1).
+
+% What is the dimension of span B1?
+% Consider the set: B2 = {z1, z2, z3, z4, z5} where
+z1 = [5; 2; 1; 7; 1];
+z2 = [2; -1; 0; 0; 1];
+z3 = [1; 2; 1; 1; 0];
+z4 = [2; -4; -2; 4; 1];
+z5 = [0; 1; 2; 3; -1];
+
+% Find all the vectors in the intersection span B1 INTERSECTION span B2.
+% Show that this is a subspace
