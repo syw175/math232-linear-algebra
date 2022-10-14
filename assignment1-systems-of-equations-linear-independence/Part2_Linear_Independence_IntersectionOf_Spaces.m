f053270e037df7b19323c533e38507754edf6186
Steven Wong <syw14@sfu.ca>
2022-10-14 14:12:59 -0700

    more work

--- assignment1-systems-of-equations-linear-independence/Part2_Linear_Independence_IntersectionOf_Spaces.m
+++ assignment1-systems-of-equations-linear-independence/Part2_Linear_Independence_IntersectionOf_Spaces.m
@@ -7,9 +7,6 @@ w5 = [5; 4; 1; 11; 3];
 w6 = [1; 0; -1; 2; 1];
 
 % Show B1 is a linearly dependent set.
-% The set of vectors in R5: B1 is linearly dependent if there is 
-% a non-trivial solution to c1w1 + c2w2 + c3w3 + c4w4 + c5w5 + c6w6 = 0
-
 % Create a system of equations using the vectors in B1
 B1 = [w1(:), w2(:), w3(:), w4(:), w5(:), w6(:)];
 zeroVectorR5 = [0; 0; 0; 0; 0];
@@ -19,11 +16,8 @@ B1Aug = cat(2, B1, zeroVectorR5);
 % Row reduce B1Aug into RREF
 B1RREF = rref(B1Aug)
 
-% The system of equations is not linearly dependent as there is a non-zero
-% solution to the system. The columns without leading 1's are linearly
-% dependent
-sprintf('As seen in the RREF of B1RREF, the system is consistent with a non-zero solution');
-sprintf('There is no leading variables for x5 and x6, they are free variables');
+% The system of equations is not linearly independent
+sprintf('The system is consistent with a non-zero solution, x5 and x6 are free');
 
 % Find a maximal linearly independent set B′1 of vectors from B1
 sprintf('The maximal linearly independent set B''1 of vectors is {w1, w2, w3, w4}')
@@ -57,6 +51,7 @@ B2 = [z1(:), z2(:), z3(:), z4(:), z5(:)];
 
 % Create an augmented matrix wih the zero vector
 B2Aug = cat(2, B2, zeroVectorR5);
+
 % Row reduce B1Aug into RREF
 B2RREF = rref(B2Aug)
 sprintf('As seen in B2RREF, the system is consistent with a non-zero solution');
@@ -73,23 +68,16 @@ B2_maximal_rref = rref(B2_maximal);
 % Find the maximal linearly independent set for both!
 % To find the intersection of the span, find the vectors in both... Set the
 % linearly combination of both spans equal to each other.
-B1B2_Maximal_System = [w1(:), w2(:), w3(:) w4(:), -z1(:), -z2(:), -z3(:), -z5(:)];
-B1B2_Aug = cat(2, B1B2_Maximal_System, zeroVectorR5);
+B1B2_Maximal_System = [w1(:), w2(:), w3(:) w4(:), -z1(:), -z2(:), -z3(:), -z5(:)]
+B1B2_Aug = cat(2, B1B2_Maximal_System, zeroVectorR5)
 B1B2_Rref = rref(B1B2_Aug)
+B1B2_Rref = rats(B1B2_Rref)
 
-% Show that this is a subspace of R5.
-% By definition, any span is a subspace. In addition, the intersection of
-% two spans will also be a subspace. However, to be more verbose, a
-% subspace is non-empty, closed under addition. and closed under scalar
-% multiplcation. It obviously contains the non-zero vector. (Say for marks)
+% Convert this with equations??
+intersection1 = rats([-7/9 ; -5/9; -4/3; -1; -4/3]);
+intersection2 = rats([-4/9; 29/18; 11/12; 3/4; 5/12;]);
+intersection3 = rats([-2/9; 5/9; -7/6; 3/2; -1/6]);
 
-% For 
 
-% For closed under addition, if we let w1 and w2 be some vectors in B1 and
-% B2, then that means that 
 
-% 
-% Show w1 + w2 is in the span of B1 and B2. if w1 and w2 are
-% in span(B2). That implies that both w1 and w2 are in the intereciton of
-% B2. that implies that w1 and w2 are in the intersection. 
-% Similarly for scalar multiplication
+
