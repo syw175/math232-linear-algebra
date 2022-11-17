be756466de36a560c3fdeafc51f72611d9d84d36
Steven Wong <syw14@sfu.ca>
2022-11-16 19:45:03 -0800

    semi-complete matlab code, ready for first draft of report writing

--- assignment3-population-dynamics/populationDynamics.m
+++ assignment3-population-dynamics/populationDynamics.m
@@ -10,6 +10,7 @@ filename = 'Population-Data.xlsx';
 populationSheet = 'Age Distribution';
 birthSheet = 'Birth Rate';
 deathSheet = 'Mortality Rates';
+immigrationSheet = 'Immigration Rate';
 
 % 1. Find age distribution, birth rate, and death rates for Canada
 % Read in our Population Data from Excel in the file Population-Data.xlsx
@@ -18,6 +19,8 @@ populationVector = readmatrix(filename, 'Sheet', populationSheet, 'Range', 'B2:B
 birthVector = readmatrix(filename, 'Sheet', birthSheet, 'Range', 'B2:B101');
 % Read in our Death Data from Excel in the file Population-Data.xlsx
 deathVector = readmatrix(filename, 'Sheet', deathSheet, 'Range', 'B2:B101');
+% Read in our Immigration Data from Excel in the file Population-Data.xlsx
+immigrationVector = readmatrix(filename, 'Sheet', immigrationSheet, 'Range', 'B2:B101');
 
 % Question why is the last col all 0s
 % 2. Formulate a discrete dynamical model as described in Boyd and Vandenberghe.
@@ -55,19 +58,19 @@ for i = 1:15
     distribution2038 = A * distribution2038;
 end
 
-% Plot the original population distribution
+% Plot the projected population distribution
 plot(populationVector, 'b');
 hold on;
 plot(distribution2028, 'r');
 hold on;
 plot(distribution2033, 'g');
 hold on;
-plot(distribution2038, 'y');
+plot(distribution2038, 'm');
 hold on;
 title('Population Distribution in Canada');
 xlabel('Age');
 ylabel('Number of People');
-legend('Original', '2028', '2033', '2038');
+legend('2023', '2028', '2033', '2038');
 
 % 4. Now make a modification of your model (i.e., the transition matrix A) 
 % incorporating immigration. Make a simple assumption of what the average 
@@ -75,5 +78,40 @@ legend('Original', '2028', '2033', '2038');
 % distribution for Canada for the years 2028, 2033, and 2038 from this
 % new model.
 
-% 5. What other factors do you think would make this model more realistic 
-% or more accurate?
+% Initialize population distribution variables
+distribution2028_Imm = populationVector;
+distribution2033_Imm = populationVector;
+distribution2038_Imm = populationVector;
+
+% Use a for loop to calculate the population distribute after 5 years using our transition matrix
+for i = 1:5
+    distribution2028_Imm = (A * distribution2028_Imm) + immigrationVector;
+end
+% Use a for loop to calculate the population distribute after 10 years using our transition matrix
+for i = 1:10
+    distribution2033_Imm = (A * distribution2033_Imm) + immigrationVector;
+end
+% Use a for loop to calculate the population distribute after 10 years using our transition matrix
+for i = 1:15
+    distribution2038_Imm = (A * distribution2038_Imm) + immigrationVector;
+end
+
+% Plot the projected population distribution with the addition of immigration rates
+plot(populationVector, 'b');
+hold on;
+plot(distribution2028_Imm, 'r')
+hold on;
+plot(distribution2033_Imm, 'g')
+hold on;
+plot(distribution2038_Imm, 'm')
+hold on;
+title('Population Distribution in Canada with addition of Immigration');
+xlabel('Age');
+ylabel('Number of People');
+legend('2023', '2028', '2033', '2038');
+
+% 5. What other factors do you think would make this model more realistic or more accurate?
+% Answer: The model could be more accurate if we were to take into account the fact that people
+% are not born at the same rate every year nor do they die at the same rate every year. There are 
+% also other factors such as emmigration that could be taken into account. Lastly the model could also
+% incorporate predictions in terms of future immigration rates.
