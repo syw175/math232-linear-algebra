5a616b3e48599bc5953c4b4bf5583d242788bbfb
Steven Wong <syw14@sfu.ca>
2022-11-19 15:29:06 -0800

    update figures to project from yr 2020 instead of 2022

--- assignment3-population-dynamics/populationDynamics.m
+++ assignment3-population-dynamics/populationDynamics.m
@@ -22,7 +22,6 @@ deathVector = readmatrix(filename, 'Sheet', deathSheet, 'Range', 'B2:B101');
 % Read in our Immigration Data from Excel in the file Population-Data.xlsx
 immigrationVector = readmatrix(filename, 'Sheet', immigrationSheet, 'Range', 'B2:B101');
 
-% Question why is the last col all 0s
 % 2. Formulate a discrete dynamical model as described in Boyd and Vandenberghe.
 % Pre-Allocate a matrix of all 0s for optimization
 A = zeros(100);
@@ -37,40 +36,38 @@ for i = 2:100
 end
 
 % 3. From your model, make plots of the age distribution in Canada for the
-% years 2028, 2033, and 2038 (5, 10, and 15 years from now).
+% years 2028, 2033, and 2038 (8, 13, and 18 years from our dataset).
 % Initialize population distribution variables
 distribution2028 = populationVector;
 distribution2033 = populationVector;
 distribution2038 = populationVector;
 
 % Use a for loop to calculate the population distribute after 5 years using our transition matrix
-for i = 1:5
+for i = 1:8
     distribution2028 = A * distribution2028;
 end
 
 % Use a for loop to calculate the population distribute after 10 years using our transition matrix
-for i = 1:10
+for i = 1:13
     distribution2033 = A * distribution2033;
 end
 
 % Use a for loop to calculate the population distribute after 10 years using our transition matrix
-for i = 1:15
+for i = 1:18
     distribution2038 = A * distribution2038;
 end
 
-% Plot the projected population distribution
-plot(populationVector, 'b');
+% Plot the projected population distribution for 2028, 2033, and 2038
+plot(distribution2028, 'b');
 hold on;
-plot(distribution2028, 'r');
+plot(distribution2033, 'r');
 hold on;
-plot(distribution2033, 'g');
+plot(distribution2038, 'g');
 hold on;
-plot(distribution2038, 'm');
-hold on;
-title('Population Distribution in Canada');
+title('Population Distribution in Canada (Excluding Immigration)');
 xlabel('Age');
-ylabel('Number of People');
-legend('2023', '2028', '2033', '2038');
+ylabel('Number of People (100,000s)');
+legend('2028', '2033', '2038');
 
 % 4. Now make a modification of your model (i.e., the transition matrix A) 
 % incorporating immigration. Make a simple assumption of what the average 
@@ -84,34 +81,26 @@ distribution2033_Imm = populationVector;
 distribution2038_Imm = populationVector;
 
 % Use a for loop to calculate the population distribute after 5 years using our transition matrix
-for i = 1:5
+for i = 1:8
     distribution2028_Imm = (A * distribution2028_Imm) + immigrationVector;
 end
 % Use a for loop to calculate the population distribute after 10 years using our transition matrix
-for i = 1:10
+for i = 1:13
     distribution2033_Imm = (A * distribution2033_Imm) + immigrationVector;
 end
 % Use a for loop to calculate the population distribute after 10 years using our transition matrix
-for i = 1:15
+for i = 1:18
     distribution2038_Imm = (A * distribution2038_Imm) + immigrationVector;
 end
 
 % Plot the projected population distribution with the addition of immigration rates
-plot(populationVector, 'b');
+plot(distribution2028_Imm, 'b')
 hold on;
-plot(distribution2028_Imm, 'r')
+plot(distribution2033_Imm, 'r')
 hold on;
-plot(distribution2033_Imm, 'g')
+plot(distribution2038_Imm, 'g')
 hold on;
-plot(distribution2038_Imm, 'm')
-hold on;
-title('Population Distribution in Canada with addition of Immigration');
+title('Population Distribution in Canada (With Immigration)');
 xlabel('Age');
-ylabel('Number of People');
-legend('2023', '2028', '2033', '2038');
-
-% 5. What other factors do you think would make this model more realistic or more accurate?
-% Answer: The model could be more accurate if we were to take into account the fact that people
-% are not born at the same rate every year nor do they die at the same rate every year. There are 
-% also other factors such as emmigration that could be taken into account. Lastly the model could also
-% incorporate predictions in terms of future immigration rates.
+ylabel('Number of People (100,000s)');
+legend('2028', '2033', '2038');
